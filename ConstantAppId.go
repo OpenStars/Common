c399f248db4729c06d21bf2222acf57b38937d3b
zserver <tunghx@sonek.vn>
2019-12-16 08:03:35 -0500

    init project

---
+++ ConstantAppId.go
@@ -0,0 +1,11 @@
+/**
+ * @author tunghx
+ * @email tunghx@sonek.vn
+ * @create date 12/15/19 9:09 PM
+ * @modify date 12/15/19 9:09 PM
+ * @desc [this package define app id for bigset database]
+ */
+
+package Common
+
+const REPORT_APP_ID int8 = 0
